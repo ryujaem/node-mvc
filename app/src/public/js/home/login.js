b85f14fb77a0035b8c5be1159228a7276f524074
jaem <jaem@lyujaemin-ui-MacBookPro.local>
2022-07-08 18:57:07 +0900

    js서버통신

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,5 +10,12 @@ function login(){
         id : id.value,
         pwd : pwd.value,
     };
-    console.log(req);
+    console.log(req,JSON.stringify(req));
+    // fetch("/login",{
+    //     method : "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(req)
+    // });
 }
