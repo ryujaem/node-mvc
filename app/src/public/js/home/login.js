89f0bb70ec0a073671e9278d320509102f0f3533
jaem <jaem@lyujaemin-ui-MacBookPro.local>
2022-07-08 15:46:06 +0900

    js로 로그인이벤트

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,14 @@
 "use strict"
 
-console.log("Hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+ pwd = document.querySelector("#pwd"),
+ btn = document.querySelector("button");
+
+btn.addEventListener("click",login);
+function login(){
+    const req = {
+        id : id.value,
+        pwd : pwd.value,
+    };
+    console.log(req);
+}
